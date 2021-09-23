72cdd1ec455767116cc4ae66b777acc1929bc809
summer-yuan <syl17762173178@163.com>
2021-09-23 14:58:21 +0800

    xiugai

--- src/main/java/com/dailyQuestion/Sept/isPowerOfThree326.java
+++ src/main/java/com/dailyQuestion/Sept/isPowerOfThree326.java
@@ -2,17 +2,13 @@ package com.dailyQuestion.Sept;
 
 public class isPowerOfThree326 {
     public boolean isPowerOfThree(int n) {
-        if(n == 3 || n == 1) {
-            return true;
-        }
-        if(n == 0) {
+        if (n <= 0) {
             return false;
         }
-        if(n%3 == 0){
-            return isPowerOfThree(n/3);
-        }else {
-            return false;
+        while (n % 3 == 0) {
+            n /= 3;
         }
-
+        return n == 1;
     }
+
 }
